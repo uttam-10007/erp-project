ee21d9cf5175deacbcdfb1a77627f392ab7d62b6
uttam-10007 <uttam@infiniseasoft.com>
2021-04-14 16:44:33 +0530

    update

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompanyModule } from './company/company.module';
+import { EmployeeComponent } from './employee/employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CompanyModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/company/company.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeloginComponent } from './employeelogin/employeelogin.component';
+
+
+
+@NgModule({
+  declarations: [EmployeeloginComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CompanyModule { }
+
+
+
